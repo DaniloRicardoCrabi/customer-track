3540283d05390bce685e38fd984f3af160a8112f
Danilo Freitas <danilo.freitas@familiapires.com.br>
2023-03-22 22:11:04 -0300

    feat: :sparkles: implementing controller sales

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { CreateSalesDto } from './dto/create-sales.dto';
+import { Sales } from './schemas/sales.schema';
+import { SalesService } from './sales.service';
 
 @Controller('sales')
-export class SalesController {}
+export class SalesController {
+    constructor(private readonly salesService: SalesService) {}
+
+    @Post()
+    async create(@Body() createSalesDto: CreateSalesDto): Promise<Sales> {
+      return this.salesService.create(createSalesDto);
+    }
+  
+    @Get()
+    async findAll(): Promise<Sales[]> {
+      return this.salesService.findAll();
+    }
+  
+    @Get(':customerId')
+    async findByCustomer(@Param('customerId') customerId: string): Promise<Sales[]> {
+      return this.salesService.findSalesByCustomer(customerId);
+    }
+
+}

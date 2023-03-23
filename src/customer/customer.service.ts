93b20752ecd561caf6d65e270db1d4f13f860020
Danilo Freitas <danilo.freitas@familiapires.com.br>
2023-03-22 22:11:59 -0300

    feat: :sparkles: create customer service

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CustomerBehaviorDto } from './dto/customer-behavior.dto';
+import { CustomerBehavior, CustomerBehaviorDocument } from './schema/customer-behavior.schema';
+
 
 @Injectable()
 export class CustomerService {
 
-    constructor(private readonly configService: ConfigService) {}
+    constructor(
+        @InjectModel(CustomerBehavior.name) private customerBehaviorModel: Model<CustomerBehaviorDocument>
+        ) {}
+
+    async create(customerBehaviorDto: CustomerBehaviorDto): Promise<CustomerBehavior> {
+        const createdCustomerBehavior = new this.customerBehaviorModel(customerBehaviorDto);
+        return createdCustomerBehavior.save();
+    }
+
+    async findByCustomerIdAndProgramId(customerId: string, programId: string ){
+        const query = { customerId, programId };
+        return this.customerBehaviorModel.find(query).exec();
+    }
+
+    async findAll(): Promise<CustomerBehavior[]> {
+        return this.customerBehaviorModel.find().exec();
+    }
+    
 }
